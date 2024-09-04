062457c872e6b1ad2e60c8849ca1d3bfe2a09c7b
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2024-09-04 16:46:35 +0300

    add main profile

--- project/main.py
+++ project/main.py
@@ -2,9 +2,13 @@ import os
 
 from loguru import logger
 
-from tools import _cls_mode_run
+from settings import load as set_load
+
+TEST = True
 
 NAME_MODULE = os.path.basename(__file__).split(".")[0]
+
+SETTINGS = {}
 
 
 def _create_log():
@@ -17,8 +21,44 @@ def _create_log():
     )
 
 
+def func_1():
+    print("func_1")
+
+
+def func_2():
+    print("func_2")
+
+
+def func_3():
+    print("func_3")
+
+
+def func_4():
+    print("func_4")
+
+
+def settings_get_profile():
+    PROFILES = {
+        "KBR_MAIN": func_1,
+        "KBR_MESSAGE": func_2,
+        "SPFS_MAIN": func_3,
+        "SPFS_MESSAGE": func_4,
+    }
+    settings_profile = SETTINGS.get("profile")
+    logger.info(settings_profile)
+    if settings_profile not in PROFILES:
+        logger.error(
+            "Неверный профиль. Укажите один из доступных:\n-> {}".format(
+                "\n-> ".join(PROFILES.keys())
+            )
+        )
+
+
 if __name__ == "__main__":
+    if TEST:
+        print("\nTEEEEEEEEESSSTT\n")
+        import pprint
     _create_log()
-    logger.info("WAW")
-    # settings.load()
-    _cls_mode_run(NAME_MODULE)
+    logger.info("Start!")
+    SETTINGS = set_load()
+    settings_get_profile()
